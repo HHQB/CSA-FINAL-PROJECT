6791bd42743fab1c84965d274ba6594b99aab042
AidenM <145621751+HHQB@users.noreply.github.com>
2026-04-28 17:20:37 +0000

    rg

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 public class Main {
+    /**
+     * Starts the cupcake game by creating a new {@code Game} instance and running
+     * it.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         // 1. Create the game instance
         Game myGame = new Game();
 
         // 2. Simulate buying buildings
-        System.out.println("Starting game...");
+        System.out.println("Starting game... Press Space then Enter to generate a cupcake");
 
         // 3. Start the game
         myGame.run();
